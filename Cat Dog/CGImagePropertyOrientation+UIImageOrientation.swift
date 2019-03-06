76baf86535039468ebeb79867b71897171ab04c8
Nima Akbarzade <Nimakbarzade@yahoo.com>
2019-03-06 21:30:55 +0330

    Model added

---
+++ Cat Dog/CGImagePropertyOrientation+UIImageOrientation.swift
@@ -0,0 +1,32 @@
+//
+//  File.swift
+//  Cat Dog
+//
+//  Created by Nima Akbarzade on 12/15/1397 AP.
+//  Copyright © 1397 AP Nima Akbarzade. All rights reserved.
+//
+
+import UIKit
+import ImageIO
+
+extension CGImagePropertyOrientation {
+    /**
+     Converts a `UIImageOrientation` to a corresponding
+     `CGImagePropertyOrientation`. The cases for each
+     orientation are represented by different raw values.
+     
+     - Tag: ConvertOrientation
+     */
+    init(_ orientation: UIImage.Orientation) {
+        switch orientation {
+        case .up: self = .up
+        case .upMirrored: self = .upMirrored
+        case .down: self = .down
+        case .downMirrored: self = .downMirrored
+        case .left: self = .left
+        case .leftMirrored: self = .leftMirrored
+        case .right: self = .right
+        case .rightMirrored: self = .rightMirrored
+        }
+    }
+}
